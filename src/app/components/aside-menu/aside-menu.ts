f37355196d6689affbfc1d7f576104956c60f6d6
Vikikos <viktoriakosarova272@gmail.com>
2026-04-13 20:09:19 +0200

    menu user modificado y pequeños ajustes de estilo

--- src/app/components/aside-menu/aside-menu.ts
+++ src/app/components/aside-menu/aside-menu.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'aside-menu-component',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './aside-menu.html',
   styleUrl: './aside-menu.css',
 })
